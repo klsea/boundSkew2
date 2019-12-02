50794e96fc9b3f2df5a824476322b7d9ff834af8
Seaman <kls190004@cvltechs-iMac-3.local>
2019-12-02 16:25:42 -0600

    Change graph color scheme

--- 01_graph_skew.R
+++ 01_graph_skew.R
@@ -1,5 +1,5 @@
 # graph skew data
-# 6.10.19 KLS
+# 6.10.19 KLS updated 12.2.19
 
 # load required packages
 library(here)
@@ -14,24 +14,28 @@ source(here('scr', 'multiplot.R'))
 # set hard-coded variables
 
 # load data
-dt <- read.csv(here("data", "bound_skew1_data.csv"))
+dt <- read.csv(here("data", "bound_skew2_data.csv"))
 
 # separate skew
-d0 <- isolate_skew(dt,c(1,2),10:69)
+d0 <- isolate_skew(dt,c(1,2),grep('X0', colnames(dt))[1]:grep('CATCH2.4', colnames(dt))) # no symmetric trials here
 d1 <- clean_skew(d0)
 
 # recode response to acceptance
 d1$accept <- d1$response - 1
 
 # reorder deg_skew factor
-d1$deg_skew <- factor(d1$deg_skew, levels = c('Symmetric', 'Weak', 'Moderate', 'Strong'))
+d1$deg_skew <- factor(d1$deg_skew, levels = c('Weak', 'Moderate', 'Strong')) # no symmetric here
 
 # make magnitude a factor
 d1$magnitude <- factor(d1$magnitude)
 
+# emulate color palette from study 1
+colors <- c('#7CAE00', '#00BFC4', '#C77CFF')
+
 # age effects on acceptance rates
 ggplot(d1, aes(Age, accept, colour = deg_skew, fill = deg_skew)) + geom_smooth(method=lm) +
-  theme(legend.position = 'top') + facet_grid(magnitude ~ valence)
+  theme(legend.position = 'top') + facet_grid(magnitude ~ valence) + 
+  scale_fill_manual(values = colors) + scale_colour_manual(values=colors)
 
 # create summary - main effect of degree of skewness
 d2 <- summarySE(data=d1, measurevar = 'accept', groupvars='deg_skew')
@@ -39,28 +43,29 @@ d2 <- summarySE(data=d1, measurevar = 'accept', groupvars='deg_skew')
 p1 <- ggplot(d2, aes(deg_skew, accept, fill = deg_skew)) + geom_bar(position=position_dodge(), stat='identity') + 
   geom_errorbar(aes(ymin=accept - se, ymax = accept + se), width = .2, position=position_dodge(.9)) + 
   theme_minimal() + xlab('Degree of Skewness') + ylab('Acceptance Rate') + expand_limits(y=1) + 
-  guides(fill=FALSE) 
+  guides(fill=FALSE) + scale_fill_manual(values = colors) + scale_colour_manual(values=colors)
 
 # create summary - add interaction with valence of gamble
 d3 <- summarySE(data=d1, measurevar = 'accept', groupvars=c('valence','deg_skew'))
 
 ggplot(d3, aes(valence, accept, fill = deg_skew)) + geom_bar(position=position_dodge(), stat='identity') + 
   geom_errorbar(aes(ymin=accept - se, ymax = accept + se), width = .2, position=position_dodge(.9)) + 
-  theme(legend.position = 'top')
+  theme(legend.position = 'top') + scale_fill_manual(values = colors) + scale_colour_manual(values=colors)
 
 # create summary - add interaction with magnitude of gamble
 d4 <- summarySE(data=d1, measurevar = 'accept', groupvars=c('magnitude','deg_skew'))
 
 ggplot(d4, aes(magnitude, accept, fill = deg_skew)) + geom_bar(position=position_dodge(), stat='identity') + 
   geom_errorbar(aes(ymin=accept - se, ymax = accept + se), width = .2, position=position_dodge(.9)) + 
-  theme(legend.position = 'top')
+  theme(legend.position = 'top') + scale_fill_manual(values = colors) + scale_colour_manual(values=colors)
 
 # create summary - 3 way interaction 
 d5 <- summarySE(data=d1, measurevar = 'accept', groupvars=c('valence', 'magnitude', 'deg_skew'))
 
 ggplot(d5, aes(valence, accept, fill = deg_skew)) + geom_bar(position=position_dodge(), stat='identity') + 
   geom_errorbar(aes(ymin=accept - se, ymax = accept + se), width = .2, position=position_dodge(.9)) + 
-  theme(legend.position = 'top') + facet_wrap(~ magnitude)
+  theme(legend.position = 'top') + facet_wrap(~ magnitude) + 
+  scale_fill_manual(values = colors) + scale_colour_manual(values=colors)
 
 # create summary - interaction between magnitude and valence 
 
@@ -79,12 +84,13 @@ d6 <- summarySE(data=d1, measurevar = 'accept', groupvars=c('magval', 'deg_skew'
 p2 <- ggplot(d6, aes(magval, accept, fill = deg_skew)) + geom_bar(position=position_dodge(), stat='identity') + 
   geom_errorbar(aes(ymin=accept - se, ymax = accept + se), width = .2, position=position_dodge(.9)) + 
   scale_fill_discrete(name = 'Degree of Skewness') + xlab('Valence by Magnitude Interaction') +
-  theme_minimal() +theme(legend.position = 'none', axis.title.y = element_blank()) + expand_limits(y=1)
+  theme_minimal() +theme(legend.position = 'none', axis.title.y = element_blank()) + expand_limits(y=1) + 
+  scale_fill_manual(values = colors) + scale_colour_manual(values=colors)
 
 p3 <- multiplot(p1, p2, layout = matrix(c(1,2,2,2), nrow=1, byrow=TRUE))
 
 #ggsave("deg_skew_plot1.pdf", plot = p3, device="pdf", path="figs/")
 
-pdf('figs/deg_skew_plot1.pdf')
+pdf('figs/deg_skew_plot2.pdf')
 multiplot(p1, p2, layout = matrix(c(1,2,2,2), nrow=1, byrow=TRUE))
 dev.off()
